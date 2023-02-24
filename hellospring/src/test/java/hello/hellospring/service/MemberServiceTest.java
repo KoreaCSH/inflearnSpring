03b49efbbb2bc3b468a34436e10e55747c2df554
csh <tmdgjs7495@naver.com>
2023-02-25 02:44:06 +0900

    mysql connect

--- hellospring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hellospring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -6,8 +6,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class MemberServiceTest {
 
