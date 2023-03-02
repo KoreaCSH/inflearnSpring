c5468a9f13c762a7a22bf586a1a57faa4f9a45bb
csh <tmdgjs7495@naver.com>
2023-03-02 22:54:04 +0900

    AppConfig를 통한 IoC, DI

--- core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -20,10 +20,10 @@ class RateDiscountPolicyTest {
         Member member = new Member(1L, "memberVIP", Grade.VIP);
 
         //when
-        int discount = discountPolicy.discount(member, 15000);
+        int discount = discountPolicy.discount(member, 20000);
 
         //then
-        assertThat(discount).isEqualTo(1500);
+        assertThat(discount).isEqualTo(2000);
     }
 
     @Test
